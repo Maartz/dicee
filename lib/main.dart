3474c3cb8ff78aa1e05185a954cbf81a08bcdad8
Maartz <maartz@protonmail.com>
2019-06-16 13:29:34 +0200

    add 2 dices

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.red.shade900,
         ),
         body: DicePage(),
       ),
@@ -18,6 +18,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
   }
 }
